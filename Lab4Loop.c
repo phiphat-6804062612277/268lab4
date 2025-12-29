1d77893bd96a3d442a09f1b50cc39f1c35ef00f2
พิพัฒน์ โชคโคกกรวด <s6804062612277@email.kmutnb.ac.th>
2025-12-29 16:26:54 +0700

    Commit 7>> lab4no10.c N grade version 3

--- Lab4Loop.c
+++ Lab4Loop.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
-	int score;
+	int score,A,B,C,D,F;
+    A = 0 ,B=0,C=0,D=0,F=0;
     while (1){
 	    scanf("%d",&score);
         if (score == -1){
@@ -9,24 +10,38 @@ int main(){
         //bonus2
 	    if (score <= 100 && score >= 0){
 		    if (score >= 68){
-			    if (score >= 85)
+			    if (score >= 85){
+                    A = A + 1;
 				    printf("(A)");
-			    else if (score >= 75)
+                }
+			    else if (score >= 75){
+                    B = B + 1;
 				    printf("(B)");
+                }
 			    else { 
+                    C = C + 1;
 				    printf("(C)");
 			    }
 		    }
 		    else {
-			    if (score >= 55)
+			    if (score >= 55){
+                    D = D + 1;
 				    printf("(D)");
-			    else 
+                }
+			    else {
+                    F = F + 1;
 				    printf("(F)");
+                }
 		    }
 	    }
 	    else 
 	    	printf("error score");
 		printf("\n");
     }
+    printf("A(%d)\n",A);
+    printf("B(%d)\n",B);
+    printf("C(%d)\n",C);
+    printf("D(%d)\n",D);
+    printf("F(%d)",F);
     return 0;
 }
