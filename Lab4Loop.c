6a04071293d351a549bb517654a3cc2cca9bb271
พิพัฒน์ โชคโคกกรวด <s6804062612277@email.kmutnb.ac.th>
2025-12-29 15:55:07 +0700

    Commit 5>> lab4no8.c N grade

--- Lab4Loop.c
+++ Lab4Loop.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 int main(){
-    char ca ;
-    int n,r ;
-    scanf("%c %d %d",&ca,&n,&r);
-    for (int iv = 0;iv<r;++iv){
-        for (int i=0;i<n;++i){
-            printf("%c",ca);
-        }
-        printf("\n");
+	int score,lo;
+    scanf("%d",&lo);
+    for (int i = 0;i < lo ;++i){
+	    scanf("%d",&score);
+        //bonus2
+	    if (score <= 100 && score >= 0){
+		    if (score >= 68){
+			    if (score >= 85)
+				    printf("(A)");
+			    else if (score >= 75)
+				    printf("(B)");
+			    else { 
+				    printf("(C)");
+			    }
+		    }
+		    else {
+			    if (score >= 55)
+				    printf("(D)");
+			    else 
+				    printf("(F)");
+		    }
+	    }
+	    else 
+	    	printf("error score");
+		printf("\n");
     }
     return 0;
 }
